a9f80bf76009506662005d5a0dae1c893549e182
riyadhsofan <riyadhsofan@gmail.com>
2019-03-06 02:54:04 +0300

    initial Commit

---
+++ test/my-module/my-module.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MyModuleComponent } from './my-module.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MyModuleComponent]
+})
+export class MyModuleModule {
+  
+ }
